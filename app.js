308107b77be05535f6212bcc9c4d53d2fedeec98
Loren Siegel <lorensiegel@Lorens-iMac.local>
2014-07-06 12:51:21 -0500

    Final

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
-	
-
-
-
-
-
-
-
+$("#numberInput").keyup(function(event) {
+		if(event.keyCode == 13) {
+			for(i=1; i<=100 ; i++){
+			if(i%15 == 0){
+				$("body").append("<p>" + "fizz buzz");
+			}
+			else if(i%5 == 0){
+				$("body").append("<p>" + "fizz");
+			}
+			else if(i%3 == 0){
+				$("body").append("<p>" + "buzz");
+			}
+			else {
+				$("body").append("<p>" + i);
+			}
+		}
+	}
 })
+});
